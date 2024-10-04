0eeee0ae720ec310ad510a03a9c39fb8a030ca92
sch990428 <990428sch@naver.com>
2024-10-04 10:40:16 +0900

    영웅 목록 UI 오류 수정
    1920 x 1080 해상도가 아닌 환경에서 실행 시 영웅 목록의 셀 크기가 다르게 표시되는 현상 수정

--- Medieval Collectible RPG/Assets/Scripts/UI/Popup/Hero/UI_HeroList.cs
+++ Medieval Collectible RPG/Assets/Scripts/UI/Popup/Hero/UI_HeroList.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_HeroList : MonoBehaviour
 {
@@ -14,10 +15,12 @@ public class UI_HeroList : MonoBehaviour
         // 플레이어가 소유한 영웅들을 표시하기 위한 슬롯을 생성하고 배치
         foreach(KeyValuePair<int, Data.CurrentPlayerOwnInfo> ownHero in LobbyManager.Instance.OwnHeroDict)
         {
-            UI_HeroSlot slot = ResourceManager.Instance.Instantiate("Prefabs/UI/Heros/UI_HeroSlotCell").GetComponent<UI_HeroSlot>();
-            slot.transform.SetParent(transform);
+            GameObject go = ResourceManager.Instance.Instantiate("Prefabs/UI/Heros/UI_HeroSlotCell");
 
-            slot.slotInfo = ownHero.Value;
+			UI_HeroSlot slot = go.GetComponent<UI_HeroSlot>();
+            slot.transform.SetParent(transform, false); // worldPositionStays를 false로 하여 로컬 방향을 유지하도록 함
+
+			slot.slotInfo = ownHero.Value;
             slot.Init();
         }
     }
