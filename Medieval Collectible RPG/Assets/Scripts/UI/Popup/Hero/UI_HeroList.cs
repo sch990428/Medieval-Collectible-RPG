09e7d356f8608d3b175095959543095f374cca3c
sch990428 <990428sch@naver.com>
2024-10-04 15:05:53 +0900

    영웅 클래스 필터링
    영웅 클래스 필터링

--- Medieval Collectible RPG/Assets/Scripts/UI/Popup/Hero/UI_HeroList.cs
+++ Medieval Collectible RPG/Assets/Scripts/UI/Popup/Hero/UI_HeroList.cs
@@ -7,21 +7,32 @@ public class UI_HeroList : MonoBehaviour
 {
     void Start()
     {
-        Init();
+		Init(0);
     }
 
-    public void Init()
+    public void Init(int typeFilter)
     {
-        // 플레이어가 소유한 영웅들을 표시하기 위한 슬롯을 생성하고 배치
-        foreach(KeyValuePair<int, Data.CurrentPlayerOwnInfo> ownHero in LobbyManager.Instance.OwnHeroDict)
+		// 기존의 슬롯들을 전부 지운다
+		foreach (Transform child in transform)
+		{
+			GameObject.Destroy(child.gameObject);
+		}
+
+		// 플레이어가 소유한 영웅들을 표시하기 위한 슬롯을 생성하고 배치
+		foreach (KeyValuePair<int, Data.CurrentPlayerOwnInfo> ownHero in LobbyManager.Instance.OwnHeroDict)
         {
-            GameObject go = ResourceManager.Instance.Instantiate("Prefabs/UI/Heros/UI_HeroSlotCell");
+            Data.HeroInfo heroinfo = LobbyManager.Instance.HeroDict[ownHero.Value.HeroId];
 
-			UI_HeroSlot slot = go.GetComponent<UI_HeroSlot>();
-            slot.transform.SetParent(transform, false); // worldPositionStays를 false로 하여 로컬 방향을 유지하도록 함
+			if (typeFilter == 0 || (typeFilter != 0 && heroinfo.HeroClass == typeFilter))
+            {
+				GameObject go = ResourceManager.Instance.Instantiate("Prefabs/UI/Heros/UI_HeroSlotCell");
 
-			slot.slotInfo = ownHero.Value;
-            slot.Init();
+				UI_HeroSlot slot = go.GetComponent<UI_HeroSlot>();
+				slot.transform.SetParent(transform, false); // worldPositionStays를 false로 하여 로컬 방향을 유지하도록 함
+
+				slot.slotInfo = ownHero.Value;
+				slot.Init();
+			} 
         }
     }
 
--- Medieval Collectible RPG/Assets/Scripts/UI/Popup/Hero/UI_HeroSlot.cs
+++ Medieval Collectible RPG/Assets/Scripts/UI/Popup/Hero/UI_HeroSlot.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
