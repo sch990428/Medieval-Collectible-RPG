a46e4148669f20af779142bc52ad852631e6d966
sch990428 <990428sch@naver.com>
2024-10-05 20:30:50 +0900

    영웅 목록 정렬 기능 추가
    등급순, 레벨순, 이름순 각각 오름차, 내림차 정렬

--- Medieval Collectible RPG/Assets/Scripts/UI/Popup/Hero/UI_HeroList.cs
+++ Medieval Collectible RPG/Assets/Scripts/UI/Popup/Hero/UI_HeroList.cs
@@ -1,6 +1,7 @@
 using Data;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,6 +15,8 @@ public class FilterData
 
 public class UI_HeroList : MonoBehaviour
 {
+	private List<UI_HeroSlot> slots;
+
 	[SerializeField]
 	private List<Color32> heroTypeColors; // 영웅 속성에 따른 슬롯 배경색
 
@@ -23,16 +26,28 @@ public class UI_HeroList : MonoBehaviour
 	[SerializeField]
 	private List<Button> typeFilterButtons; // 영웅 속성에 따른 필터링 버튼
 
+	[SerializeField]
+	private TMP_Dropdown sortDropdown; // 영웅 정렬 옵션을 선택하는 토글 버튼
+
+	[SerializeField]
+	private Toggle toggleAsending; // 정렬 순서(오름, 내림)를 선택하는 토글버튼
+
 	UI_HeroSlot slot;
 	private FilterData filterData;
 
 	void Start()
 	{
+		slots = new List<UI_HeroSlot>();
 		filterData = new FilterData() { ClassFilter = 0, TypeFilter = 0 };
+		sortDropdown.onValueChanged.AddListener(UpdateSort);
+		toggleAsending.onValueChanged.AddListener(UpdateAscending);
+
 		UpdateClassFilter(0);
 		UpdateTypeFilter(0);
+
 		Init();
-    }
+		UpdateSort(0); // 최초 정렬 호출
+	}
 
     public void Init()
     {
@@ -49,8 +64,14 @@ public class UI_HeroList : MonoBehaviour
 			slot.HeroTypeColor = heroTypeColors[heroinfo.HeroType];
 			slot.slotInfo = ownHero.Value;
 			slot.Init();
+
+			slots.Add(slot);
         }
-    }
+
+		// 정렬을 위한 버튼들에 값변경 리스너 추가
+		sortDropdown.onValueChanged.AddListener(UpdateSort);
+		toggleAsending.onValueChanged.AddListener(UpdateAscending);
+	}
 
 	// 영웅의 직군 별로 선택값에 따라 필터링한다
 	public void UpdateClassFilter(int filter)
@@ -94,7 +115,7 @@ public class UI_HeroList : MonoBehaviour
 		UpdateFilter();
 	}
 
-	// 필터에 따라 영웅 슬롯들을 갱신한다
+	// 필터에 따라 영웅 슬롯들의 활성화 상태를 갱신한다
 	public void UpdateFilter()
 	{
 		foreach(Transform slot in transform)
@@ -106,6 +127,111 @@ public class UI_HeroList : MonoBehaviour
 			bool typeCondition = filterData.TypeFilter == 0 || hero.HeroType == filterData.TypeFilter;
 
 			slot.gameObject.SetActive(classCondition && typeCondition);
+		}
+	}
+
+	// 이름순 정렬
+	public int CompareHerosByName(UI_HeroSlot slotA, UI_HeroSlot slotB, bool isAsending = false)
+	{
+		Data.HeroInfo heroA = LobbyManager.Instance.HeroDict[slotA.slotInfo.HeroId];
+		Data.HeroInfo heroB = LobbyManager.Instance.HeroDict[slotB.slotInfo.HeroId];
+
+		int nameComparison = isAsending? heroB.HeroName.CompareTo(heroA.HeroName) : heroA.HeroName.CompareTo(heroB.HeroName);
+
+		// 이름이 같다면 등급으로 비교
+		if (nameComparison == 0)
+		{
+			int gradeComparison = slotB.slotInfo.HeroGrade.CompareTo(slotA.slotInfo.HeroGrade);
+
+			if (gradeComparison == 0)
+			{
+				// 등급도 같으면 레벨로 비교
+				return slotB.slotInfo.HeroLevel.CompareTo(slotA.slotInfo.HeroLevel);
+			}
+
+			return gradeComparison;
+		}
+
+		return nameComparison; // 등급이 다르면 등급 기준으로 정렬
+	}
+
+	// 등급순 정렬
+	public int CompareHerosByGrade(UI_HeroSlot slotA, UI_HeroSlot slotB, bool isAsending = false)
+	{
+		Data.HeroInfo heroA = LobbyManager.Instance.HeroDict[slotA.slotInfo.HeroId];
+		Data.HeroInfo heroB = LobbyManager.Instance.HeroDict[slotB.slotInfo.HeroId];
+
+		int gradeComparison = isAsending ? slotB.slotInfo.HeroGrade.CompareTo(slotA.slotInfo.HeroGrade) : slotA.slotInfo.HeroGrade.CompareTo(slotB.slotInfo.HeroGrade);
+
+		// 등급이 같다면 레벨로 비교
+		if (gradeComparison == 0)
+		{
+			int levelComparison = slotB.slotInfo.HeroLevel.CompareTo(slotA.slotInfo.HeroLevel);
+
+			if (levelComparison == 0)
+			{
+				// 레벨도 같으면 이름으로 비교
+				return heroA.HeroName.CompareTo(heroB.HeroName);
+			}
+
+			return levelComparison;
+		}
+
+		return gradeComparison; // 등급이 다르면 등급 기준으로 정렬
+	}
+
+	// 레벨순 정렬
+	public int CompareHerosByLevel(UI_HeroSlot slotA, UI_HeroSlot slotB, bool isAsending = false)
+	{
+		Data.HeroInfo heroA = LobbyManager.Instance.HeroDict[slotA.slotInfo.HeroId];
+		Data.HeroInfo heroB = LobbyManager.Instance.HeroDict[slotB.slotInfo.HeroId];
+
+		int levelComparison = isAsending ? slotB.slotInfo.HeroLevel.CompareTo(slotA.slotInfo.HeroLevel) : slotA.slotInfo.HeroLevel.CompareTo(slotB.slotInfo.HeroLevel);
+
+		// 레벨이 같다면 등급으로 비교
+		if (levelComparison == 0)
+		{
+			int gradeComparison = slotB.slotInfo.HeroGrade.CompareTo(slotA.slotInfo.HeroGrade);
+
+			if (gradeComparison == 0)
+			{
+				// 등급도 같으면 이름으로 비교
+				return heroA.HeroName.CompareTo(heroB.HeroName);
+			}
+
+			return gradeComparison;
+		}
+
+		return levelComparison; // 등급이 다르면 등급 기준으로 정렬
+	}
+
+	// 정렬 순서 변경시 호출되는 중간함수
+	public void UpdateAscending(bool toggle)
+	{
+		UpdateSort(sortDropdown.value);
+	}
+
+	// 기준에 따라 영웅 슬롯들을 정렬한다
+	public void UpdateSort(int option)
+	{
+		switch (option)
+		{
+			case 0:
+				slots.Sort((a, b) => CompareHerosByGrade(a, b, toggleAsending.isOn));
+				break;
+
+			case 1:
+				slots.Sort((a, b) => CompareHerosByLevel(a, b, toggleAsending.isOn));
+				break;
+
+			case 2:
+				slots.Sort((a, b) => CompareHerosByName(a, b, toggleAsending.isOn));
+				break;
+		}
+
+		for (int i = 0; i < slots.Count; i++)
+		{
+			slots[i].transform.SetSiblingIndex(i);
 		}
 	}
 
