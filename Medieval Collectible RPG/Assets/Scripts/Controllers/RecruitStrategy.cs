730516ae70ba1824368049855bfcdda9c672d4e0
sch990428 <990428sch@naver.com>
2024-10-14 11:44:48 +0900

    영웅 소환에 전략패턴 적용
    영웅 소환에 전략패턴 적용

---
+++ Medieval Collectible RPG/Assets/Scripts/Controllers/RecruitStrategy.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IRecruitStrategy
+{
+    public List<int> Recruit(int count);
+}
+
+// 일반 소환
+public class GeneralRecruitStrategy : IRecruitStrategy
+{
+	public List<int> Recruit(int count)
+	{
+		List<int> recruitResult = new List<int>();
+		int totalHeroNum = LobbyManager.Instance.HeroDict.Count;
+
+		for (int i = 0; i < count; i++)
+		{
+			int random = UnityEngine.Random.Range(1, 11);
+
+			if (random == 1 || random == 2)
+			{
+				int whichHero = UnityEngine.Random.Range(1, totalHeroNum + 1);
+				recruitResult.Add(whichHero);
+			}
+			else
+			{
+				recruitResult.Add(0);
+			}
+		}
+
+		return recruitResult;
+	}
+}
+
+// 라이언 픽업소환
+public class Hero1PickUpRecruitStrategy : IRecruitStrategy
+{
+	public List<int> Recruit(int count)
+	{
+		List<int> recruitResult = new List<int>();
+		int totalHeroNum = LobbyManager.Instance.HeroDict.Count;
+
+		for (int i = 0; i < count; i++)
+		{
+			int random = UnityEngine.Random.Range(1, 11);
+
+			if (random == 1 || random == 2)
+			{
+				int whichHero = UnityEngine.Random.Range(1, totalHeroNum + 2);
+				if (whichHero == totalHeroNum + 1)
+				{
+					whichHero = 1;
+				}
+				recruitResult.Add(whichHero);
+			}
+			else
+			{
+				recruitResult.Add(0);
+			}
+		}
+
+		return recruitResult;
+	}
+}
